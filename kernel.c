d665e4ea774cc2222a7fc985e14b028c2e104130
m1cky <mabir4@students.d125.org>
2014-01-29 23:58:45 -0600

    Simple putc() prototype

--- kernel.c
+++ kernel.c
@@ -1,4 +1,26 @@
-int kmain(struct multiboot *mboot_ptr)
+/* system.h */
+
+#define VIDMEM 0xB8000
+
+typedef unsigned int    u32;
+typedef           int    s32;
+typedef unsigned short u16;
+typedef           short s16;
+typedef unsigned char   u8;
+typedef           char   s8;
+
+/* system.h */
+
+u8* location;
+
+void putc(u8 c, u8 fg, u8 bg)
 {
-  return 0xDEADBABA;
+  location = VIDMEM;
+  *location = c | (((bg << 4) | fg) << 8);
+}
+
+int kmain()
+{
+  putc('P', 0, 15);
+  return 0;
 }
